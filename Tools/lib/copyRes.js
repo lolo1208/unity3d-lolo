41c2b97e2a06e9bf7399ed9107384c5b97ee194b
LOLO <154160595@qq.com>
2018-10-24 15:04:14 +0800

    change tools

--- Tools/lib/copyRes.js
+++ Tools/lib/copyRes.js
@@ -1,5 +1,7 @@
-var args = require("./node_modules/commander");
-var fs = require("fs");
+//
+
+const fs = require('fs');
+const args = require('../node_modules/commander');
 
 
 args.android = false;
@@ -8,39 +10,37 @@ args
     .option('-a, --android', 'true：将资源拷贝到Android Studio项目资源目录，false[默认]：将资源拷贝到Xcode项目资源目录')
     .parse(process.argv);
 
-var isAndroid = args.android;
-var RES_DIR = __dirname + "/../../Assets/StreamingAssets/";
-var TAR_DIR = __dirname + "/../../PlatformProjects/" + (isAndroid
-            ? "AndroidStudio/src/main/assets/"
-            : "Xcode/Data/Raw/"
+const ANDROID = args.android;
+const RES_DIR = __dirname + '/../../Assets/StreamingAssets/';
+const TAR_DIR = __dirname + '/../../PlatformProjects/' + (ANDROID
+            ? 'AndroidStudio/src/main/assets/'
+            : 'Xcode/Data/Raw/'
     );
 
 
 //
 
-// Copy Res
-if (isAndroid) copyDir(TAR_DIR + "bin/", TAR_DIR + "../bin/");// 先把bin目录拷贝至上层
+
+//
+// copy res
+//
+let [binDir, pbinDir] = [`${TAR_DIR}bin/`, `${TAR_DIR}../bin/`];
+if (ANDROID) copyDir(binDir, pbinDir);// 先把bin目录拷贝至上层
 removeDir(TAR_DIR);
 copyDir(RES_DIR, TAR_DIR);
-if (isAndroid) {
-    copyDir(TAR_DIR + "../bin/", TAR_DIR + "bin/");// 拷回bin目录
-    removeDir(TAR_DIR + "../bin/");
-    console.log("Copy Res to Android Studio Project Completed!");
+if (ANDROID) {
+    copyDir(pbinDir, binDir);// 拷回bin目录
+    removeDir(pbinDir);
+    console.log('Copy Res to Android Studio Project Completed!');
 } else {
-    console.log("Copy Res to Xcode Project Completed!");
+    console.log('Copy Res to Xcode Project Completed!');
 }
+
 
 //
 
 
-/**
- * str 的结束内容是否为 endValue
- * @param str
- * @param endValue
- */
-function endsWith(str, endValue) {
-    return str.substring(str.length - endValue.length) === endValue;
-}
+//
 
 
 /**
@@ -51,12 +51,12 @@ function endsWith(str, endValue) {
 function copyDir(oldDir, newDir) {
     oldDir = formatDirPath(oldDir);
     newDir = formatDirPath(newDir);
-    var files = fs.readdirSync(oldDir);
+    let files = fs.readdirSync(oldDir);
     if (files.length === 0) return;
     createDir(newDir);
-    for (var i = 0; i < files.length; i++) {
-        var oldFile = oldDir + files[i];
-        var newFile = newDir + files[i];
+    for (let i = 0; i < files.length; i++) {
+        let oldFile = oldDir + files[i];
+        let newFile = newDir + files[i];
         if (fs.statSync(oldFile).isDirectory())
             copyDir(oldFile, newFile);
         else
@@ -70,11 +70,11 @@ function copyDir(oldDir, newDir) {
  * @param newFile
  */
 function copyFile(oldFile, newFile) {
-    if (endsWith(oldFile, ".meta") || endsWith(oldFile, ".manifest")
-        || endsWith(oldFile, ".DS_Store") || endsWith(oldFile, "TestModeFlag")
+    if (oldFile.endsWith('.meta') || oldFile.endsWith('.manifest')
+        || oldFile.endsWith('.DS_Store') || oldFile.endsWith('TestModeFlag')
     ) return;
 
-    var buffer = fs.readFileSync(oldFile);
+    let buffer = fs.readFileSync(oldFile);
     fs.writeFileSync(newFile, buffer);
 }
 
@@ -83,12 +83,12 @@ function copyFile(oldFile, newFile) {
  * @param path
  */
 function createDir(path) {
-    path = path.replace(/\\/g, "/");
-    var arr = path.split("/");
+    path = path.replace(/\\/g, '/');
+    let arr = path.split('/');
     path = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i] === "") continue;
-        path += "/" + arr[i];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === '') continue;
+        path += '/' + arr[i];
         if (!fs.existsSync(path)) {
             fs.mkdirSync(path);
         }
@@ -100,11 +100,11 @@ function createDir(path) {
  * @param path
  */
 function removeDir(path) {
-    var files = [];
+    let files = [];
     if (fs.existsSync(path)) {
         files = fs.readdirSync(path);
-        files.forEach(function (file/*, index*/) {
-            var curPath = path + "/" + file;
+        files.forEach(function (file, index) {
+            let curPath = path + '/' + file;
             if (fs.statSync(curPath).isDirectory()) {
                 removeDir(curPath);
             } else {
@@ -121,8 +121,8 @@ function removeDir(path) {
  */
 function formatDirPath(dirPath) {
     if (dirPath === null) return null;
-    dirPath = dirPath.replace(/\\/g, "/");
-    if (dirPath.substring(dirPath.length - 1) !== "/") dirPath += "/";
+    dirPath = dirPath.replace(/\\/g, '/');
+    if (dirPath.substring(dirPath.length - 1) !== '/') dirPath += '/';
     return dirPath;
 }
 
