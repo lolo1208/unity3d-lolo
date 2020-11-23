a13ef08f0107f42f3a9555435e1e12316a43d495
LOLO <154160595@qq.com>
2020-11-23 17:13:29 +0800

    Update Config.lua

--- Assets/Lua/Data/Config.lua
+++ Assets/Lua/Data/Config.lua
@@ -5,19 +5,16 @@
 --
 
 
-
 local Config = {}
 
+
+--=------------------[ C#LanguageWindow 修改 ]------------------=--
 --- 使用的语种地区代码
 Config.language = "en-US"
-
---- 是否记录网络通信日志
-Config.logNetEnabled = true
+--=-------------------------------------------------------------=--
 
 
-
--- ------------------[ C#LocalizationText 调用 ]------------------ --
-
+--=------------------[ C#LocalizationText 调用 ]------------------=--
 --- 获取当前使用的语种地区代码
 function Config._GetCurrentLanguage()
     return Config.language
@@ -27,7 +24,13 @@ end
 function Config._GetLanguageByKey(key)
     return Language[key]
 end
+--=---------------------------------------------------------------=--
 
+
+
+--
+--- 是否记录网络通信日志
+Config.logNetEnabled = true
 
 
 
