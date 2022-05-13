8ae3664c3d5f3d336711a3403406c834ad33828c
LOLO <154160595@qq.com>
2022-05-13 16:54:41 +0800

    Update config.js

--- Tools/lib/config/config.js
+++ Tools/lib/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
 
     // unity 程序路径
     macUnityPath: '/Applications/Unity/Hub/Editor/[UnityVersion]/Unity.app/Contents/MacOS/Unity',
-    winUnityPath: '"C:/Program Files/Unity/Hub/Editor/[UnityVersion]/Editor/Unity.exe"',
+    winUnityPath: '"C:/Program Files/Unity/[UnityVersion]/Editor/Unity.exe"',
 
     // 默认的 Unity 版本（也可以使用 -u 参数传入）
     unityVersion: '2020.3.12f1',
