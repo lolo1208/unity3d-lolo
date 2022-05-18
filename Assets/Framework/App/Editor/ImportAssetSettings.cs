6281410bcac0451f785e8b39dd380354c0c00ee2
LOLO <154160595@qq.com>
2022-05-18 20:47:38 +0800

    Android use ASTC

--- Assets/Framework/App/Editor/ImportAssetSettings.cs
+++ Assets/Framework/App/Editor/ImportAssetSettings.cs
@@ -15,135 +15,146 @@ namespace App
     /// </summary>
     public class ImportAssetSettings : AssetPostprocessor
     {
-        /// Android 平台名称
-        private const string PLATFORM_ANDROID = "Android";
-        /// iOS 平台名称
-        private const string PLATFORM_IOS = "iPhone";
+        /// 目标平台名称 iOS Android
+        private static readonly string[] Platforms = { "iPhone", "Android" };
         /// 已导入过的资源标记
         private const string UD_IMPORTED = "Imported";
 
 
 
-        /// <summary>
-        /// 当前处理的资源是否已经导入过了
-        /// </summary>
-        /// <returns><c>true</c>, if ignored was caned, <c>false</c> otherwise.</returns>
-        private bool IsImported()
-        {
-            if (Application.isBatchMode) return true;
-            if (!assetPath.StartsWith(Constants.ResDirPath, StringComparison.Ordinal)) return true;
-            if (assetImporter.userData == UD_IMPORTED)
-            {
-                Debug.Log(assetPath + " 已被导入过，本次导入过程将会被忽略！若要恢复默认设置，请右键点击该文件或目录，然后点击菜单项 [Reimport With Default Setting]");
-                return true;
-            }
-            return false;
-        }
+        #region 纹理相关设置 和 spritePackingTag
 
-
-        /// <summary>
-        /// 标记该资源已经被导入过了
-        /// </summary>
-        private void MarkImported()
-        {
-            assetImporter.userData = UD_IMPORTED;
-        }
-
-
-
-        /// <summary>
-        /// 设置纹理资源格式和 spritePackingTag
-        /// </summary>
         void OnPreprocessTexture()
         {
             if (Application.isBatchMode) return;
-
             if (Path.GetExtension(assetPath).ToLower() == ".dds")
             {
                 LogError("警告：iOS 不支持 .dds 格式的纹理！" + assetPath);
                 return;
             }
-
-            TextureImporter importer = (TextureImporter)assetImporter;
-            // Sprite Texture（目录或文件名是否包含 UI 字符）
-            bool isSprite = assetPath.Contains("UI/") || assetPath.Contains("UI_") || assetPath.Contains("_UI");
-
-            // 验证纹理宽高是否为 4 的倍数（Android ETC2）
-            if (!isSprite)
-            {
-                Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(importer.assetPath);
-                if (texture == null && importer.textureShape == TextureImporterShape.Texture2D)
-                {
-                    importer.SaveAndReimport();// 加载纹理数据失败（添加新文件）时，重新再来
-                    return;
-                }
-                importer.spritePackingTag = "";
-
-                if (texture != null && (texture.width % 4 != 0 || texture.height % 4 != 0))
-                    LogWarning(assetPath + " 的宽高不是 4 的倍数，这将导致在打包 Android 时，无法压缩该图像！");
-            }
-
             if (IsImported()) return;
 
-            // 根据目录来设置 spritePackingTag
-            if (isSprite)
+            TextureImporter importer = (TextureImporter)assetImporter;
+            importer.isReadable = false;
+
+            // ASTC 支持情况
+            //   iOS: A8 - iphone6, iPad mini 4 开始支持
+            //   Android: 部分 OpenGL ES 3.0 开始支持
+            TextureImporterFormat textureFormat = importer.DoesSourceTextureHaveAlpha() ? TextureImporterFormat.ASTC_6x6 : TextureImporterFormat.ASTC_8x8;
+            TextureCompressionQuality compressionQuality = TextureCompressionQuality.Normal;
+            string upperPath = assetPath.ToUpper();
+            string upperName = Path.GetFileNameWithoutExtension(upperPath);
+            int maxTextureSize = 0;
+            bool isSingleChannel = false;
+            bool overridden = true;
+
+
+            // UI - 目录或文件名包含 "UI" 字符
+            if (upperPath.Contains("UI/") || upperPath.Contains("UI_") || upperPath.Contains("_UI"))
             {
                 importer.textureType = TextureImporterType.Sprite;
                 importer.mipmapEnabled = false;
-
-                string fileName = Path.GetFileName(assetPath);
+                textureFormat = TextureImporterFormat.ASTC_6x6;
+                // 根据目录来设置 spritePackingTag
                 string tag = assetPath
                     .Replace(Constants.ResDirPath, "")
-                    .Replace("/" + fileName, "")
+                    .Replace("/" + Path.GetFileName(assetPath), "")
                     .Replace("/", "_");
                 importer.spritePackingTag = tag;
-                Debug.Log(assetPath + " set spritePackingTag: " + tag);
+                Debug.Log(assetPath + " [spritePackingTag]: " + tag);
+            }
+            else
+            {
+                importer.textureType = TextureImporterType.Default;
+                importer.spritePackingTag = "";
+                importer.mipmapEnabled = true;
+                importer.streamingMipmaps = true;
+                maxTextureSize = 1024;
+
+                // 按文件名后缀来识别贴图类型，忽略大小写
+                // Diffuse - 漫反射/颜色 贴图
+                if (upperName.EndsWith("_D", StringComparison.Ordinal))
+                {
+                    importer.sRGBTexture = true;
+                    compressionQuality = TextureCompressionQuality.Best;
+                }
+
+                // Normal - 法线贴图
+                else if (upperName.EndsWith("_N", StringComparison.Ordinal))
+                {
+                    importer.textureType = TextureImporterType.NormalMap;
+                    importer.sRGBTexture = true;
+                    compressionQuality = TextureCompressionQuality.Best;
+                    textureFormat = TextureImporterFormat.ASTC_5x5;
+                }
+
+                // Distort - 光照贴图
+                else if (upperName.EndsWith("_LIGHTMAP", StringComparison.Ordinal))
+                {
+                    importer.textureType = TextureImporterType.Lightmap;
+                    // overridden = false;
+                }
+
+                // Mask - 蒙版/遮罩 贴图
+                else if (upperName.EndsWith("_M", StringComparison.Ordinal))
+                {
+                    importer.sRGBTexture = true;
+                    compressionQuality = TextureCompressionQuality.Best;
+                }
+
+                // Thickness - 厚度贴图
+                else if (upperName.EndsWith("_THICKNESS", StringComparison.Ordinal))
+                {
+                    isSingleChannel = true;
+                    maxTextureSize = 128;
+                }
+
+                // Distort - 扭曲/变形 贴图
+                else if (upperName.EndsWith("_DISTORT", StringComparison.Ordinal))
+                {
+                    isSingleChannel = true;
+                    maxTextureSize = 128;
+                }
             }
 
-            // 设置各平台压缩格式
-            SetTexturePlatformSetting(importer, PLATFORM_IOS);
-            SetTexturePlatformSetting(importer, PLATFORM_ANDROID);
-            importer.isReadable = false;
+            // 单通道贴图设置
+            if (isSingleChannel)
+            {
+                importer.textureType = TextureImporterType.SingleChannel;
+                TextureImporterSettings importerSetting = new TextureImporterSettings();
+                importer.ReadTextureSettings(importerSetting);
+                importerSetting.singleChannelComponent = TextureImporterSingleChannelComponent.Red;
+                importer.SetTextureSettings(importerSetting);
+            }
+
+
+            // 设置 Android 与 iOS
+            foreach (string platform in Platforms)
+            {
+                TextureImporterPlatformSettings platformSettings = importer.GetPlatformTextureSettings(platform);
+                TextureImporterPlatformSettings settings = new TextureImporterPlatformSettings
+                {
+                    allowsAlphaSplitting = platformSettings.allowsAlphaSplitting,
+                    crunchedCompression = platformSettings.crunchedCompression,
+                    name = platformSettings.name,
+                    resizeAlgorithm = platformSettings.resizeAlgorithm,
+                    textureCompression = TextureImporterCompression.Compressed,
+                    compressionQuality = (int)compressionQuality,
+                    maxTextureSize = maxTextureSize > 0 ? maxTextureSize : platformSettings.maxTextureSize,
+                    overridden = overridden
+                };
+                settings.format = textureFormat;
+                importer.SetPlatformTextureSettings(settings);
+            }
             MarkImported();
         }
 
-
-        /// <summary>
-        /// 设置纹理指定平台的格式，默认压缩
-        /// </summary>
-        /// <param name="importer">Importer.</param>
-        /// <param name="platform">Platform.</param>
-        private void SetTexturePlatformSetting(TextureImporter importer, string platform)
-        {
-            TextureImporterPlatformSettings platformSettings = importer.GetPlatformTextureSettings(platform);
-            TextureImporterPlatformSettings settings = new TextureImporterPlatformSettings
-            {
-                allowsAlphaSplitting = platformSettings.allowsAlphaSplitting,
-                crunchedCompression = platformSettings.crunchedCompression,
-                maxTextureSize = platformSettings.maxTextureSize,
-                name = platformSettings.name,
-                resizeAlgorithm = platformSettings.resizeAlgorithm,
-
-                textureCompression = TextureImporterCompression.Compressed,
-                compressionQuality = (int)UnityEditor.TextureCompressionQuality.Normal,
-                overridden = true
-            };
-
-            // 注：升级到 Unity 2019 使用 URP 时，ASTC_RGBA_6x6 和 ASTC_RGB_6x6 将不可用，
-            //    可使用 ASTC_6x6 来代替这两个值，Unity 会自动判定 alpha 来做压缩。
-            bool isAndroid = platform == PLATFORM_ANDROID;
-            if (importer.DoesSourceTextureHaveAlpha())
-                settings.format = isAndroid ? TextureImporterFormat.ETC2_RGBA8 : TextureImporterFormat.ASTC_6x6;
-            else
-                settings.format = isAndroid ? TextureImporterFormat.ETC2_RGB4 : TextureImporterFormat.ASTC_6x6;
-            importer.SetPlatformTextureSettings(settings);
-        }
+        #endregion
 
 
 
-        /// <summary>
-        /// 音频的压缩相关设置
-        /// </summary>
+        #region 音频压缩相关设置
+
         void OnPreprocessAudio()
         {
             if (IsImported()) return;
@@ -190,30 +201,81 @@ namespace App
             MarkImported();
         }
 
+        #endregion
 
 
-        /// <summary>
-        /// 模型，动画的压缩相关设置
-        /// </summary>
+
+        #region 模型，动画 相关设置
+
         void OnPreprocessModel()
         {
             if (IsImported()) return;
-
             ModelImporter importer = assetImporter as ModelImporter;
-            importer.animationCompression = ModelImporterAnimationCompression.Optimal;
-            importer.meshCompression = ModelImporterMeshCompression.Medium;
-            //importer.importNormals = ModelImporterNormals.None;
-            //importer.importMaterials = false;
+
+            // Scene
+            importer.useFileScale = true;
+            importer.importVisibility = false;
+            importer.importCameras = false;
+            importer.importLights = false;
+            importer.preserveHierarchy = false;
+            importer.sortHierarchyByName = false;
+
+            // Mesh
+            importer.meshCompression = ModelImporterMeshCompression.Off;// 开启该选项可节省本地磁盘空间，与运行时无关
             importer.optimizeMeshPolygons = true;
             importer.optimizeMeshVertices = true;
-            //importer.optimizeGameObjects = true;
+            importer.addCollider = false;
             importer.isReadable = false;
+
+            // Geometry
+            importer.importNormals = ModelImporterNormals.Import;
+            importer.importBlendShapeNormals = ModelImporterNormals.Import;
+            importer.keepQuads = false;
+            importer.weldVertices = true;
+            // importer.generateSecondaryUV = true;
+
+            // Animation
+            importer.animationCompression = ModelImporterAnimationCompression.Optimal;
+
             MarkImported();
         }
+
+        #endregion
+
+
+
+        #region 在 AssetImporter 中标记该资源是否已导入过
+
+        /// <summary>
+        /// 当前处理的资源是否已经导入过了
+        /// </summary>
+        /// <returns><c>true</c>, if ignored was caned, <c>false</c> otherwise.</returns>
+        private bool IsImported()
+        {
+            if (Application.isBatchMode) return true;
+            if (!assetPath.StartsWith(Constants.ResDirPath, StringComparison.Ordinal)) return true;
+            if (assetImporter.userData == UD_IMPORTED)
+            {
+                Debug.Log(assetPath + " 已被导入过，本次导入过程将会被忽略！若要恢复默认设置，请右键点击该文件或目录，然后点击菜单项 [Reimport With Default Setting]");
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 标记该资源已经被导入过了
+        /// </summary>
+        private void MarkImported()
+        {
+            assetImporter.userData = UD_IMPORTED;
+        }
+
+        #endregion
 
 
 
         #region 在运行时动态更新（re-require）lua 文件
+
 #if REINPORT_LUA_ON_PLAYING
 
         /// 动态更新的 lua 代码
@@ -247,6 +309,7 @@ namespace App
         }
 
 #endif
+
         #endregion
 
 
