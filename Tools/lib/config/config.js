b433fd0913f0f6c34c31b8fd22f63f9a3eb5cbda
LOLO <154160595@qq.com>
2022-06-07 10:48:25 +0800

    Update config.js

--- Tools/lib/config/config.js
+++ Tools/lib/config/config.js
@@ -6,7 +6,7 @@
 module.exports = {
 
     // unity 程序路径
-    macUnityPath: '/Applications/Unity/Hub/Editor/[UnityVersion]/Unity.app/Contents/MacOS/Unity',
+    macUnityPath: '/Applications/Unity/[UnityVersion]/Unity.app/Contents/MacOS/Unity',
     winUnityPath: '"C:/Program Files/Unity/[UnityVersion]/Editor/Unity.exe"',
 
     // 默认的 Unity 版本（也可以使用 -u 参数传入）
